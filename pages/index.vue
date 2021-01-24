d09fd98f1c1b92f6d7c6549416a3c9f81dd30b89
missmarcus <39994983+missmarcus@users.noreply.github.com>
2021-01-24 22:33:39 +0100

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+ .uk-height-viewport.uk-flex.uk-flex-middle.uk-flex-center 
+    button.uk-button.uk-button-default.uk-border-pill Thismedium.com
+    
+</template>
+<script>
+export default {
+    head() {
+        return {
+            link: [
+                {rel:"stylesheet", href: 'https://cdn.jsdelivr.net/npm/uikit@3.6.13/dist/css/uikit.min.css'}
+            ]
+        }
+    }
+}
+</script>
