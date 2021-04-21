fa70cc716921d42ec5bfa633b220e36483b93a5a
Omotola Bolarin <bolarin@omoto.la>
2021-04-21 23:24:46 +0200

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,4 +2,15 @@ export default {
     // ssr: false, 
     // target: 'static',
     srcDir: 'src',
+    head: {
+      title: 'nuxt',
+      meta: [
+        { charset: 'utf-8 ' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'hello' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ],
+    },
 }
