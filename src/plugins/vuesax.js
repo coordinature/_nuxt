dc6e4014e3d1abfd9a74e56c374293644f64a791
Omotola Bolarin <bolarin@omoto.la>
2021-04-22 01:26:51 +0200

    Update vuesax.js

--- src/plugins/vuesax.js
+++ src/plugins/vuesax.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import { vsButton, vsSelect, vsPopup } from 'vuesax'
+// import { vsButton, vsSelect, vsPopup } from 'vuesax'
 // import 'vuesax/dist/vuesax.css'
 // import 'boxicons/css/boxicons.min.css'
 
-Vue.use(vsButton)
+/* Vue.use(vsButton)
 Vue.use(vsSelect)
-Vue.use(vsPopup)
+Vue.use(vsPopup) */
