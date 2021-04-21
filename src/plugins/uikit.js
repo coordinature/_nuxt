0b4666dff44399af343c6ccab9d8dc9d8a036965
Omotola Bolarin <bolarin@omoto.la>
2021-04-22 01:01:26 +0200

    Update uikit.js

--- src/plugins/uikit.js
+++ src/plugins/uikit.js
@@ -1,6 +1,6 @@
 import UIkit from 'uikit'
-import Icons from 'uikit/dist/js/uikit-icons'
+// import Icons from 'uikit/dist/js/uikit-icons'
 // import 'uikit/dist/css/uikit.min.css'
 
-UIkit.use(Icons)
+// UIkit.use(Icons)
 UIkit.container = '#__nuxt'
