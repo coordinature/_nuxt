71e9c1f2c1a22a213787d16e117e2c5994fb6f4b
automotola <bolarin@omoto.la>
2021-05-18 12:41:42 +0200

    clean with ui-card
    We've created a group conversation.
    We've cleaned the code up, by following the DRY protocol.
    We've added a ui-kit container/grid/card.
    This sets a good base to load in the sidebar, from our previous design.

--- src/components/inbox.vue
+++ src/components/inbox.vue
@@ -1,77 +1,122 @@
 <template lang="pug">
-div#talkjs-container(style='width: 90%; margin: 30px; height: 500px')
-    i Loading chat...
+.uk-flex
+    #talkjs-container.uk-width-expand(style="width: 90%; margin: 30px 0; height: 550px")
+        i Loading chat...
+    .uk-panel.uk-width-medium.uk-background-muted.uk-padding-small
+        .uk-card.uk-card-small.uk-card-default
+            .uk-card-header
+                span.uk-text-small hello
+            .uk-card-body hello body
 </template>
 <script>
 import Talk from "talkjs";
 
-(function(t,a,l,k,j,s){
-s=a.createElement('script');s.async=1;s.src="https://cdn.talkjs.com/talk.js";a.head.appendChild(s)
-;k=t.Promise;t.Talk={v:3,ready:{then:function(f){if(k)return new k(function(r,e){l.push([f,r,e])});l
-.push([f])},catch:function(){return k&&new k()},c:l}};})(window,document,[]);
-
 export default {
     name: 'Inbox',
-        props: {
-            currentUser: {
-                type: Object,
-                //required: true,
-                default: function() {
-                    return {
-                        id: '129223',
-                        name: 'Jane Doe',
-                        email: 'jane@example.nl',
-                        photoUrl: 'https://demo.talkjs.com/img/sebastian.jpg',
-                        welcomeMessage: 'Hello',
-                    }
+
+    props: {
+        currentUser: {
+            type: Object,
+            //required: true,
+            default: function() {
+                return {
+                    id: '654321',
+                    name: 'Current User',
+                    email: 'jane@example.nl',
+                    photoUrl: null,
+                    welcomeMessage: 'Hello from currentUser',
+                    role: "Initiator"
                 }
             }
-        },
-    mounted() {
-       /* const currentUser = {
-                id: this.currentUser.id,
-                name: this.currentUser.name,
-                email: this.currentUser.email,
-                photoUrl: this.currentUser.photoUrl,
-                welcomeMessage: "Hi, how are you?",
-                role: "parent"
-            }*/
-        Talk.ready.then(function() {
-            var me = new Talk.User({
-                id: parseInt(Math.random()*500000).toString(),
-                name: "Sanne",
-                email: "Sanne@example.nl",
-                photoUrl: "https://eu.ui-avatars.com/api/?name=Sanne+Doe",
-                welcomeMessage: "Hey there! How are you? :-)",
-                role: "Initiator"
-            });
-            var talkSession = new Talk.Session({
-                appId: "tRm5PSRh",
-                me: me
-            });
-            var other = new Talk.User({
-                id: "654321",
-                name: "Jack",
-                email: "jack@example.com",
-                photoUrl: "https://eu.ui-avatars.com/api/?name=Jack+Doe",
-                welcomeMessage: "Hi there, how can I help?",
-                role: "FamilyMember"
-            })
-            var other2 = new Talk.User({
-                id: "654321",
-                name: "Rick",
-                email: "Rick@example.com",
-                photoUrl: "https://eu.ui-avatars.com/api/?name=Rick+Doe",
-                welcomeMessage: "Hi there, how can I help?",
-                role: "FamilyMember"
-            })
-            var conversation = talkSession.getOrCreateConversation(Talk.oneOnOneId(me, other));
-            conversation.setParticipant(me);
-            conversation.setParticipant(other);
-            var inbox = talkSession.createInbox({selected: conversation});
-            inbox.mount(document.getElementById("talkjs-container"));
+        }
+    },
 
-        })
+    data() {
+        return {
+            user: [],
+            contacts: []
+        }
+    },
+        
+    mounted() {
+        this.user = {
+            id: this.currentUser.id,
+            name: this.currentUser.name,
+            email: this.currentUser.email,
+            photoUrl: this.currentUser.photoUrl || `https://eu.ui-avatars.com/api/?name=${this.currentUser.name}`,
+            welcomeMessage: this.currentUser.welcomeMessage,
+            role: this.currentUser.role
+        }
+
+        this.contacts = [
+            {
+                    id: "jack",
+                    name: "Jack",
+                    email: "jack@example.com",
+                    photoUrl: null,
+                    welcomeMessage: "Hi there, how can I help?",
+                    role: "FamilyMember"
+            },
+            {
+                    id: 'rick',
+                    name: "Rick",
+                    email: "Rick@example.com",
+                    photoUrl: null,
+                    welcomeMessage: "Hi there, how can I help?",
+                    role: "FamilyMember"
+            },
+        ]
+
+        this.initConversation(this.user, this.contacts, 'ResearchStarter')
+
+    },
+
+    methods: {
+        initConversation(user, contacts, conversationId) {
+            Talk.ready.then(function() {
+                var me = new Talk.User(user);
+                
+                /* var privateSession = new Talk.Session({
+                    appId: "tRm5PSRh",
+                    me: me
+                });*/
+
+                // Create a session with me in it
+                const session = new Talk.Session({
+                    appId: "tRm5PSRh",
+                    me: me
+                });
+
+                // Create a conversation in this session
+                const conversation = session.getOrCreateConversation(conversationId);
+                conversation.setAttributes({
+                    photoUrl: `https://eu.ui-avatars.com/api/?name=research`,
+                    subject: "Research Starter"
+                });
+
+                // conversation.setParticipant(me);
+
+                contacts.forEach(contact => {
+                    conversation.setParticipant(new Talk.User(contact));
+                })
+                               
+
+                var inbox = session.createInbox({selected: conversation});
+                inbox.mount(document.getElementById("talkjs-container"));
+
+                /* var privateConversation = privateSession.getOrCreateConversation(Talk.oneOnOneId(me, other1));
+                privateConversation.setParticipant(me);
+                privateConversation.setParticipant(other1);
+                var inbox = talkSession.createInbox({selected: privateConversation}); */
+
+                const element = document.getElementsByClassName('NoticeBar')
+                console.log("notice hidden", element)
+
+            })
+
+            
+        
+        },
     }
     
 }
--- src/pages/messages.vue
+++ src/pages/messages.vue
@@ -1,3 +1,3 @@
 <template lang="pug">
-inbox
+inbox.uk-height-viewport
 </template>
