cf90f30d87fe7f159d8ef26add906e90631486c2
automotola <bolarin@omoto.la>
2021-05-18 12:41:42 +0200

    clean with ui-card
    We've created a group conversation.
    We've cleaned the code up, by following the DRY protocol.
    We've added a ui-kit container/grid/card.
    This sets a good base to load in the sidebar, from our previous design.

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,6 +13,9 @@ export default {
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       ],
+      script: [
+        { src: 'https://cdn.talkjs.com/talk.js' }
+      ]
     },
     css: [
       // 'uikit/dist/css/uikit.min.css',
