36648c81b60ebd9f368e301de301f5b1ca4ca791
automotola <bolarin@omoto.la>
2021-08-31 02:06:34 +0200

    Adapted to @thismedium

--- src/plugins/uikit.js
+++
@@ -1,6 +0,0 @@
-import UIkit from 'uikit'
-import Icons from 'uikit/dist/js/uikit-icons'
-import 'uikit/dist/css/uikit.min.css'
-
-UIkit.use(Icons)
-UIkit.container = '#__nuxt'
--- src/plugins/vuesax.js
+++
@@ -1,8 +0,0 @@
-// import Vue from '../../../../tm-mini/node_modules/vue/types'
-// import { vsButton, vsSelect, vsPopup } from 'vuesax'
-// import 'vuesax/dist/vuesax.css'
-// import 'boxicons/css/boxicons.min.css'
-
-/* Vue.use(vsButton)
-Vue.use(vsSelect)
-Vue.use(vsPopup) */
