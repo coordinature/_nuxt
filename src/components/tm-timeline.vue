47f0a2578db0c7c2c029a0d84a804d4336d8221c
missmarcus <39994983+missmarcus@users.noreply.github.com>
2021-09-21 13:24:09 +0200

    ant design timeline
    it's not working just yet

---
+++ src/components/tm-timeline.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+div
+  a-timeline
+    a-timeline-item Oktober 2021 - KPN families only
+    a-timeline-item January 2022 - Beta test for early adopters
+    a-timeline-item March 2022 - public beta test
+</template>
+<script>
+export default {
+  name: 'Tmtimeline',
+
+}
+</script>
